e808618c91e87f3812d39a79e71494cd3ef1504a
r000909 <r000909@pc-000909-6.local>
2019-12-14 10:53:45 +0900

    add device info

--- front/src/js/script.js
+++ front/src/js/script.js
@@ -2,27 +2,27 @@
 const btns = document.getElementsByClassName('btn');
 const url = 'https://sv51qzcvzb.execute-api.ap-northeast-1.amazonaws.com/dev/BB-devCampStep3-Okinawa-LT-func';
 
-const postRecord = function(VAL) {
-  return axios.post(url, {answer: VAL});
+const postRecord = function(ANS, DEVICE) {
+  return axios.post(url, {answer: ANS, device: DEVICE});
 };
 
 for (let i = 0; i < btns.length; i++) {
   btns[i].onmouseup = function() {
-    console.log(this.textContent);
+    const device = window.navigator.userAgent;
 
-    return postRecord(this.textContent)
-    .then(() => {
-      swal({
-        title: 'kintoneへ登録しました！',
-        text: `【${this.textContent}】`,
-        icon: 'success',
+    return postRecord(this.textContent, device)
+      .then(() => {
+        swal({
+          title: 'kintoneへ登録しました！',
+          text: `【${this.textContent}】`,
+          icon: 'success',
+        });
+      }).catch(err => {
+        swal({
+          title: '登録に失敗しました！',
+          text: `【${err.message}】`,
+          icon: 'error',
+        });
       });
-    }).catch(err => {
-      swal({
-        title: '登録に失敗しました！',
-        text: `【${err.message}】`,
-        icon: "error",
-      });
-    })
-  }
+  };
 }
