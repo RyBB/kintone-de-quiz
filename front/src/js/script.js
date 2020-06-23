9a1cf224cd65272b2a631912f271cff662295754
BB <ryo-otake@cybozu.co.jp>
2020-06-23 14:57:10 +0900

    Update script.js

--- front/src/js/script.js
+++ front/src/js/script.js
@@ -1,25 +1,25 @@
 'use strict';
-const btns = document.getElementsByClassName('vote-btn');
-const url = 'https://gyem8jkda8.execute-api.ap-northeast-1.amazonaws.com/dev/BB-devCamp-Step3-LT-WebVote-func';
+var btns = document.getElementsByClassName('vote-btn');
+var url = 'https://gyem8jkda8.execute-api.ap-northeast-1.amazonaws.com/dev/BB-devCamp-Step3-LT-WebVote-func';
 
-const postRecord = function(ANS, DEVICE) {
+var postRecord = function(ANS, DEVICE) {
   return axios.post(url, {answer: ANS, device: DEVICE});
 };
 
-for (let i = 0; i < btns.length; i++) {
+for (var i = 0; i < btns.length; i++) {
   btns[i].onmouseup = function() {
-    const device = window.navigator.userAgent;
+    var device = window.navigator.userAgent;
     return postRecord(this.textContent, device)
-      .then(() => {
+      .then(function() {
         swal({
           title: 'kintoneへ登録しました！',
-          text: `【${this.textContent}】`,
+          text: '【' + this.textContent + '】',
           icon: 'success',
         });
-      }).catch(err => {
+      }).catch(function(err) {
         swal({
           title: '登録に失敗しました！',
-          text: `【${err.message}】`,
+          text: '【' + err.message + '】',
           icon: 'error',
         });
       });
